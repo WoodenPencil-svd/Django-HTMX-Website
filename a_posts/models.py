3e65ef0b764f4a465f369a65f71edc62752db204
Thanh Nhan <gin21010304@gmail.com>
2024-06-19 15:38:20 +0700

    19/6

--- a_posts/models.py
+++ a_posts/models.py
@@ -10,3 +10,5 @@ class Post(models.Model):
     
     def __str__(self):
         return str(self.title)
+    class Meta:
+        ordering =['-created']  # Make the new post go up
--- core/urls.py
+++ core/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from a_posts.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('a_posts.urls'))
+    path('', home_view, name = 'home'),
+    path('post/create', post_create_view,name='post-create'),
 ]
