6208a745702c5490c8b8b26ff950e0d041a79379
Thanh Nhan <gin21010304@gmail.com>
2024-06-20 14:53:30 +0700

    20/6

--- a_posts/views.py
+++ a_posts/views.py
@@ -1,30 +1,61 @@
-from django.shortcuts import render,redirect
-from .models import *
+from django.shortcuts import render, redirect
+from .models import Post
 from django.forms import ModelForm
 from django import forms
+from bs4 import BeautifulSoup
+import requests
 
+# View to display all posts
 def home_view(request):
     posts = Post.objects.all()
-    return render(request,'a_posts/home.html', {'posts': posts})
+    return render(request, 'a_posts/home.html', {'posts': posts})
 
+# Form class for creating a new post
 class PostCreateForm(ModelForm):
     class Meta:
-       model = Post
-       fields = '__all__'
-       labels = {
-           'body':'Caption'
-       }
-       widgets = {
-           'body' : forms.Textarea(attrs={'row':3, 'placeholder': 'Add a caption... ', 'class': 'font1 text-4xl' }),
-       }
+        model = Post
+        fields = ['url','body']
+        labels = {
+            'body': 'Caption'
+        }
+        widgets = {
+            'body': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Add a caption...', 'class': 'font1 text-4xl'}),
+            'url': forms.TextInput(attrs={'placeholder': 'Add url...'}),
+         
+        }
 
+# View to create a new post
 def post_create_view(request):
     form = PostCreateForm()
-    
+
     if request.method == 'POST':
         form = PostCreateForm(request.POST)
         if form.is_valid():
-            form.save()
+            post = form.save(commit=False)
+
+            
+            website = requests.get(form.cleaned_data['url'])
+            sourcecode = BeautifulSoup(website.text, 'html.parser')
+
+           
+            find_image = sourcecode.select('img.main-photo')
+            image = find_image[0]['src']
+            post.image = image
+
+
+           
+            find_title = sourcecode.select(' h1.meta-field.photo-title')
+            title = find_title[0].text.strip()
+            post.title = title
+
+            
+            
+            find_artist = sourcecode.select('a.owner-name.truncate')
+            artist = find_artist[0].text.strip()
+            post.artist = artist
+
+           
+            post.save()
             return redirect('home')
-        
-    return render(request, 'a_posts/post_create.html',{ 'form': form })
+
+    return render(request, 'a_posts/post_create.html', {'form': form})
